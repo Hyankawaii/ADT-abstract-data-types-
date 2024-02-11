94f6ec86075298d9263d086234e69d09c5951b40
Hyankawaii <125204317+Hyankawaii@users.noreply.github.com>
2024-02-11 16:15:22 +0700

    some might be wrong btw

---
+++ BinarySearch.py
@@ -0,0 +1,9 @@
+set = [0,1,2,3,4,5,6] # declare set : ARRAY[0:6] OF INTEGER
+high = len[set]
+low = 0
+def BinarySearch(value):
+    high = len[set]
+    low = 0
+    mid = (high - low) // 2
+    found = False
+    while found = False
